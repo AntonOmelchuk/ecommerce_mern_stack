501eb78c877626c9bdc10aeda4c48fbbf7ea4514
AntonOmelchuk <kino_kg@ukr.net>
2020-11-03 22:33:34 +0200

    init template, navigation

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Header from './components/nav/Header'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Home from './pages/home/Home'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Switch>
+    </>
+  )
+}
+
+export default App
