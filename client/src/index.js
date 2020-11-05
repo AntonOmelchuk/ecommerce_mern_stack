999246e0a57ddbec8c7f59e0305856534ca5adac
Anton Omelchuk <kino_kg@ukr.net>
2020-11-05 21:49:53 +0200

    setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import 'antd/dist/antd.css'
+import store from './store'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
 
