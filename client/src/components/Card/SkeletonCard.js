926ef0851649b91e83b1ac712d4de238d177a399
Anton Omelchuk <kino_kg@ukr.net>
2020-11-26 16:58:10 +0200

    add skeleton card

---
+++ client/src/components/Card/SkeletonCard.js
@@ -0,0 +1,21 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Card, Skeleton } from 'antd'
+
+const SkeletonCard = ({ count }) => {
+  const CustomCard = () => (
+    <Card className='col m-3'>
+      <Skeleton active />
+    </Card>
+  )
+  return (
+    Array.from(Array(count), (_, i) => <CustomCard key={i} />)
+  )
+}
+
+SkeletonCard.propTypes = {
+  count: PropTypes.number.isRequired,
+}
+
+export default SkeletonCard
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,33 +1,35 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { getProducts } from '../../actions/product'
+
 import ProductCard from '../../components/Card/ProductCard'
 import Jumbotron from '../../components/Jumbotron/Jumbotron'
-import LoadingTitle from '../../components/LoadingTitle/LoadingTitle'
+import SkeletonCard from '../../components/Card/SkeletonCard'
+
+import { getProducts } from '../../actions/product'
 
 const Home = () => {
+  const PRODUCT_COUNT = 10
   const dispatch = useDispatch()
   const { product: { products }, general: { loading } } = useSelector(state => state)
 
   useEffect(() => {
-    dispatch(getProducts(10))
-    return () => {}
+    dispatch(getProducts(PRODUCT_COUNT))
   }, [dispatch])
 
   return (
     <>
       <div className='jumbotron text-danger h1 font-weight-bold text-center'>
-        {loading
-          ? <LoadingTitle loading={loading} />
-          : <Jumbotron text={['Latest Products', 'New arrivals', 'Best Sellers']} />}
+        <Jumbotron text={loading ? ['Loading...'] : ['Latest Products', 'New arrivals', 'Best Sellers']} />
       </div>
       <div className='container'>
         <div className='row'>
-          {products.map(product => (
-            <div key={product._id} className='col-md-4'>
-              <ProductCard product={product} />
-            </div>
-          ))}
+          {loading
+            ? <SkeletonCard count={3} />
+            : products.map(product => (
+              <div key={product._id} className='col-md-4'>
+                <ProductCard product={product} />
+              </div>
+            ))}
         </div>
       </div>
     </>
