06455146411c411b04a6cc3662f1221795ed6b0d
Anton Omelchuk <kino_kg@ukr.net>
2020-11-26 16:05:21 +0200

    add products card to Home page

---
+++ client/src/components/Card/ProductCard.js
@@ -0,0 +1,61 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Card } from 'antd'
+import { EyeOutlined, ShoppingCartOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+
+const { Meta } = Card
+
+const ProductCard = ({ product }) => {
+  const {
+    title, description, images, slug
+  } = product
+  const descr = description.length > 43 ? `${description.substring(0, 42)}...` : description
+  return (
+    <Card
+      cover={images.length ? (
+        <img src={images[0].url} alt='product' style={{ height: '180px', objectFit: 'cover' }} className='p-1' />
+      ) : (
+        <h1 style={{
+          height: '162px', textAlign: 'center', marginBottom: 0, paddingTop: '18px'
+        }}
+        >
+          no image
+        </h1>
+      )}
+      className='m-2'
+      actions={[
+        <Link to={`/product/${slug}`}>
+          <EyeOutlined className='text-info' />
+          {' '}
+          <br />
+          {' '}
+          View Product
+        </Link>,
+        <>
+          <ShoppingCartOutlined className='text-info' />
+          {' '}
+          <br />
+          {' '}
+          Add to Cart
+        </>
+      ]}
+    >
+      <Meta title={title} description={descr} />
+    </Card>
+  )
+}
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.shape({
+      url: PropTypes.string.isRequired
+    })),
+  }).isRequired,
+}
+
+export default ProductCard
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProducts } from '../../actions/product'
+import ProductCard from '../../components/Card/ProductCard'
+import LoadingTitle from '../../components/LoadingTitle/LoadingTitle'
 
 const Home = () => {
-  return <div>Home</div>
+  const dispatch = useDispatch()
+  const { product: { products }, general: { loading } } = useSelector(state => state)
+
+  useEffect(() => {
+    dispatch(getProducts(10))
+    return () => {}
+  }, [dispatch])
+
+  return (
+    <>
+      <div className='jumbotron'>
+        <LoadingTitle title='All Products' loading={loading} />
+      </div>
+      <div className='container'>
+        <div className='row'>
+          {products.map(product => (
+            <div key={product._id} className='col-md-4'>
+              <ProductCard product={product} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Home
