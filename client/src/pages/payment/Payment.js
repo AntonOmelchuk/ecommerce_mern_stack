9596cba7c7f33b94e1f7535ddf47d17b504db522
Anton Omelchuk <kino_kg@ukr.net>
2020-12-09 21:47:58 +0200

    config stripe on client side

--- client/src/pages/payment/Payment.js
+++ client/src/pages/payment/Payment.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_SECRET)
 
 const Payment = () => {
   return (
     <div className='container p-5 text-center'>
-      <p>Complete your purchase</p>
+      <Elements stripe={promise}>
+        <div className='col-md-8 offset-md-2'>
+          <p>Complete your purchase</p>
+        </div>
+      </Elements>
     </div>
   )
 }
